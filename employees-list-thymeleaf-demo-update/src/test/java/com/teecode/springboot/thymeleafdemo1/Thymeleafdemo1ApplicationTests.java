059d5340b9199958ea58f34f89d82c1effd46b7f
TolulopeOredein <38234469+TolulopeOredein@users.noreply.github.com>
2021-10-11 00:45:05 +0100

    Add files via upload

---
+++ employees-list-thymeleaf-demo-update/src/test/java/com/teecode/springboot/thymeleafdemo1/Thymeleafdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teecode.springboot.thymeleafdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Thymeleafdemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
